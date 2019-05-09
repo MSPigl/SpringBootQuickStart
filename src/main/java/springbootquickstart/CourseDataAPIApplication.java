00728153ec84be044231eccac315225b2f8ae864
MSPigl <mattep96@gmail.com>
2019-05-09 16:07:29 -0400

    Add files

---
+++ src/main/java/springbootquickstart/CourseDataAPIApplication.java
@@ -0,0 +1,11 @@
+package springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDataAPIApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDataAPIApplication.class, args);
+	}
+}
